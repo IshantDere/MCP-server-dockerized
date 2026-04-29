e4cb750a17cfd3a3ebd46b93e92ef56da9a06869
IshantDere <Ishantdere06@gmail.com>
2026-04-29 18:07:19 +0530

    Update client.py

--- client.py
+++ client.py
@@ -3,7 +3,7 @@ from fastmcp import Client
 
 
 async def main() -> None:
-    client = Client("http://136.111.109.186:8000/mcp")
+    client = Client("http://localhost:8000/mcp")
     async with client:
         result = await client.call_tool("add", {"a": 20, "b": 33})
         print(result)
